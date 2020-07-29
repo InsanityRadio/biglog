a37bc8f8c416e58d88b64cecd59590c398a51693
Jamie Woods <jamiewoods@Jamies-MacBook-Pro.local>
2020-07-29 21:17:11 +0100

    Initial commit

---
+++ biglog.js
@@ -0,0 +1,12 @@
+import Rotter from './lib/rotter';
+import path from 'path';
+
+const options = {
+	directory: path.resolve('./logs'),
+	format: '%S-%X.mp3'
+}
+
+new Rotter('http://contrib.insn.it:8000/sra-split-a.mp3', 'sra-split-a', options);
+new Rotter('http://contrib.insn.it:8000/sra-split-b.mp3', 'sra-split-b', options);
+new Rotter('http://contrib.insn.it:8000/sra-split-c.mp3', 'sra-split-c', options);
+new Rotter('http://contrib.insn.it:8000/sra-split-d.mp3', 'sra-split-d', options);

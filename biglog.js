33fa8cc28a2dda1045a727b546a965b4ff57ab9f
Jamie Woods <jamiewoods@Jamies-MacBook-Pro.local>
2020-07-29 22:47:46 +0100

    update config syntax

--- biglog.js
+++ biglog.js
@@ -1,13 +1,14 @@
-import Rotter from './lib/rotter';
-import path from 'path';
-import fs from 'fs';
+const path = require('path');
+const fs = require('fs');
+
+const Rotter = require('./lib').default;
 
 const options = {
 	directory: path.resolve('./logs') + '/%S-%D',
 	format: '%S-%X.mp3'
 }
 
-let config;
+let config, stations;
 let rotters = {}
 
 /*
@@ -18,8 +19,11 @@ function start () {
 	try {
 		// Cautiously read the new configuration file
 		config = JSON.parse(fs.readFileSync('biglog.json'))
-
+		
+		Object.assign(options, config.options || {})
+		stations = config.stations;
 	} catch (e) {
+		console.log(e)
 		console.log('!! Invalid configuration file. Oh no')
 		// yeet out early so we don't change anything
 		return;
@@ -28,12 +32,12 @@ function start () {
 	console.log('Configuration file is reloading')
 
 	let dirty = Object.keys(rotters);
-	config.forEach((stn) => dirty.indexOf(stn.name) >= 0 && delete dirty[dirty.indexOf(stn.name)])
+	stations.forEach((stn) => dirty.indexOf(stn.name) >= 0 && delete dirty[dirty.indexOf(stn.name)])
 	dirty = dirty.filter((d) => d);
 
 	dirty.forEach((dirty) => { rotters[dirty].close(); delete rotters[dirty]; })
 
-	config.map((stn) => {
+	stations.map((stn) => {
 		if (rotters[stn.name]) {
 			return;
 		}

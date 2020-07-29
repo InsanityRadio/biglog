619656d8eff269d04a7fbafc628d62a7028aed77
Jamie Woods <jamiewoods@Jamies-MacBook-Pro.local>
2020-07-29 22:19:49 +0100

    Add cool config.json

--- biglog.js
+++ biglog.js
@@ -1,12 +1,36 @@
 import Rotter from './lib/rotter';
 import path from 'path';
+import fs from 'fs';
 
 const options = {
 	directory: path.resolve('./logs') + '/%S-%D',
 	format: '%S-%X.mp3'
 }
 
-new Rotter('http://contrib.insn.it:8000/sra-split-a.mp3', 'sra-split-a', options);
-new Rotter('http://contrib.insn.it:8000/sra-split-b.mp3', 'sra-split-b', options);
-new Rotter('http://contrib.insn.it:8000/sra-split-c.mp3', 'sra-split-c', options);
-new Rotter('http://contrib.insn.it:8000/sra-split-d.mp3', 'sra-split-d', options);
+let config;
+let rotters = {}
+
+function start () {
+	try {
+		config = JSON.parse(fs.readFileSync('biglog.json'))
+	} catch (e) {
+		console.log('!! Invalid configuration file. Oh no')
+		return;
+	}
+	let dirty = Object.keys(rotters);
+	config.forEach((stn) => dirty.indexOf(stn.name) >= 0 && delete dirty[dirty.indexOf(stn.name)])
+	dirty = dirty.filter((d) => d);
+
+	dirty.length && console.log('Closing rotters', dirty)
+
+	dirty.forEach((dirty) => { rotters[dirty].close(); delete rotters[dirty]; })
+
+	config.map((stn) => {
+		if (rotters[stn.name]) return;
+		rotters[stn.name] = new Rotter(stn.src, stn.name, options)
+	})
+
+}
+start();
+
+fs.watchFile('biglog.json', start);
